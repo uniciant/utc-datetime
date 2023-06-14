4ccc22b02364b3feb5c0358bbe435d044ff60b32
Reece Kibble <reecek@uniciant.com>
2023-06-15 01:09:05 +0800

    time.rs: remove redundant checks, add unit tests

--- src/time.rs
+++ src/time.rs
@@ -8,17 +8,21 @@ use anyhow::{
 use crate::date::UTCDate;
 
 const SECONDS_PER_DAY: u64 = 86400;
+const MILLIS_PER_DAY: u64 = SECONDS_PER_DAY * 1000;
+const MICROS_PER_DAY: u64 = MILLIS_PER_DAY * 1000;
+const NANOS_PER_DAY: u64 = MICROS_PER_DAY * 1000;
 const MILLIS_PER_SECOND: u64 = 1000;
 const MICROS_PER_SECOND: u64 = MILLIS_PER_SECOND * 1000;
 const NANOS_PER_SECOND: u64 = MICROS_PER_SECOND * 1000;
 
 /// UTC Timestamp.
 /// A UTC Timestamp the duration since the Unix Epoch (1970).
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct UTCTimestamp(Duration);
 
 impl UTCTimestamp {
     pub fn to_time_of_day_nanos(&self) -> u64 {
-        (self.0.as_secs() * NANOS_PER_SECOND) + (self.0.subsec_nanos() as u64)
+        ((self.0.as_secs() % SECONDS_PER_DAY)  * NANOS_PER_SECOND) + (self.0.subsec_nanos() as u64)
     }
 
     pub fn to_utc_days(&self) -> u32 {
@@ -34,50 +38,52 @@ impl UTCTimestamp {
     }
 
     pub fn try_from_components(secs: u64, nanos: u32) -> Result<Self> {
-        if nanos >= (NANOS_PER_SECOND as u32) {
-            return Err(anyhow!("Nanoseconds should total less than a second! (nanos: {})", nanos));
-        }
         Ok(Self(Duration::new(secs, nanos)))
     }
 
-    /// Reference:
-    /// http://howardhinnant.github.io/date_algorithms.html#days_from_civil
-    ///
-    /// Simplified for unsigned days/years
-    pub fn try_from_date_and_nanos(year: u32, month: u8, day: u8) -> Result<Self> {
-        let m = month as u32;
-        let d = day as u32;
-        let y = year - ((m <= 2) as u32);
-        let era = y / 400;
-        let yoe = y;
-        let doy = ((153 * (if m > 2 { m - 3 } else { m + 9 }) + 2) / 5) + d - 1;
-        let doe = (yoe * 365) + (yoe / 4) + (yoe / 100) + doy;
-        let utc_day = ((era * 146097) + doe) - 719468;
-        Self::try_from_days_and_nanos(utc_day, 0u32)
-    }
-
-    pub fn try_from_days_and_nanos<T: Into<u64>, U: Into<u32>>(d: T, ns: U) -> Result<Self> {
-        let secs = d.into() * SECONDS_PER_DAY;
-        let nanos = ns.into();
+    pub fn try_from_days_and_nanos(days: u32, time_of_day_ns: u64) -> Result<Self> {
+        if time_of_day_ns >= NANOS_PER_DAY {
+            return Err(anyhow!("Nanoseconds not within a day! (time_of_day_ns: {})", time_of_day_ns));
+        }
+        let secs = (days as u64 * SECONDS_PER_DAY) + (time_of_day_ns / NANOS_PER_SECOND);
+        let nanos = (time_of_day_ns % NANOS_PER_SECOND) as u32;
         Self::try_from_components(secs, nanos)
     }
 
-
-    pub fn from_millis<T: Into<u64>>(ms: T) -> Self {
-        Self(Duration::from_millis(ms.into()))
+    pub fn from_millis(ms: u64) -> Self {
+        Self(Duration::from_millis(ms))
     }
 
-    pub fn from_micros<T: Into<u64>>(us: T) -> Self {
-        Self(Duration::from_micros(us.into()))
+    pub fn from_micros(us: u64) -> Self {
+        Self(Duration::from_micros(us))
     }
 
-    pub fn from_nanos<T: Into<u64>>(ns: T) -> Self {
-        Self(Duration::from_nanos(ns.into()))
+    pub fn from_nanos(ns: u64) -> Self {
+        Self(Duration::from_nanos(ns))
     }
 }
 
 impl From<UTCDate> for UTCTimestamp {
     fn from(date: UTCDate) -> Self {
-        Self::try_from_date_and_nanos(date.year(), date.month(), date.day()).unwrap()
+        Self::try_from_days_and_nanos(date.to_utc_days(), 0).unwrap()
     }
+}
+
+#[test]
+fn test_from_days_and_nanos() -> Result<()> {
+    let test_cases = [
+        (UTCTimestamp(Duration::from_nanos(0)), 0, 0),
+        (UTCTimestamp(Duration::from_nanos(123456789)), 0, 123456789),
+        (UTCTimestamp(Duration::from_millis(1686756677000)), 19522, 55877_000_000_000),
+        (UTCTimestamp(Duration::from_millis(1709220677000)), 19782, 55877_000_000_000),
+        (UTCTimestamp(Duration::from_millis(1677684677000)), 19417, 55877_000_000_000),
+        (UTCTimestamp(Duration::new(u32::MAX as u64 * SECONDS_PER_DAY, 0)), u32::MAX, 0),
+    ];
+
+    for (expected, utc_day, time_of_day_ns)  in test_cases {
+        let timestamp = UTCTimestamp::try_from_days_and_nanos(utc_day, time_of_day_ns)?;
+        assert_eq!(timestamp, expected);
+    }
+
+    Ok(())
 }
