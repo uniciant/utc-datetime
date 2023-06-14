32a2f8152fc15553c584d99413480802495cc913
Reece Kibble <reecek@uniciant.com>
2023-06-14 22:44:29 +0800

    date.rs: implement date unit tests

--- src/date.rs
+++ src/date.rs
@@ -5,7 +5,7 @@ use anyhow::{
 
 use crate::time::UTCTimestamp;
 
-#[derive(Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct UTCDate {
     year: u32,
     month: u8,
@@ -20,11 +20,14 @@ impl UTCDate {
             month,
             day,
         };
-        if date.month == 0 || date.month > 12 {
-            return Err(anyhow!("Month out of range! (month: {})", month));
+        if date.year < 1970 {
+            return Err(anyhow!("Year out of range! (year: {:04})", year))
         }
-        if date.day > date.days_in_month() {
-            return Err(anyhow!("Day out of range! (day: {}) (yyyy-mm: {}-{:02})", day, month, year));
+        if date.month == 0 || date.month > 12 {
+            return Err(anyhow!("Month out of range! (month: {:02})", month));
+        }
+        if date.day == 0 || date.day > date.days_in_month() {
+            return Err(anyhow!("Day out of range! (day: {:02}) (yyyy-mm: {:04}-{:02})", day, month, year));
         }
         Ok(date)
     }
@@ -81,7 +84,7 @@ impl UTCDate {
             1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
             4 | 6 | 9 | 11 => 30,
             2 => if self.is_leap_year() { 29 } else { 28 },
-            _ => panic!("Month out of range! {}", self.month)
+            _ => panic!("Month out of range! {:2}", self.month)
         }
     }
 }
@@ -90,4 +93,60 @@ impl From<UTCTimestamp> for UTCDate {
     fn from(timestamp: UTCTimestamp) -> Self {
         Self::from_utc_days(timestamp.to_utc_days())
     }
+}
+
+#[test]
+fn test_utc_date_from_components() -> Result<()> {
+    let test_cases = [
+        (2023, 6, 14, true),  // valid recent date
+        (1970, 1, 1, true), // valid epoch date
+        (2024, 2, 29, true), // valid leap day
+        (1969, 12, 31, false), // invalid before epoch
+        (2023, 2, 29, false), // invalid date
+        (2023, 0, 10, false), // invalid date, month out of range
+        (2023, 13, 10, false), // invalid date, month out of range
+        (2023, 9, 31, false), // invalid date, day out of range
+        (2023, 9, 0, false), // invalid date, day out of range
+    ];
+
+    for (year, month, day, case_is_valid) in test_cases {
+        match UTCDate::try_from_components(year, month, day) {
+            Ok(_) => {
+                if !case_is_valid {
+                    return Err(anyhow!("Case passed unexpectedly. (date: {:04}-{:02}-{:02})", year, month, day));
+                }
+                continue;
+            },
+            Err(e) => {
+                if case_is_valid {
+                    return Err(e);
+                }
+                continue;
+            }
+        }   
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_from_utc_days() -> Result<()> {
+    let test_cases = [
+        (0, 1970, 1, 1),
+        (30, 1970, 1, 31),
+        (19522, 2023, 6, 14),
+        (381112, 3013, 6, 14),
+    ];
+
+    for (day_utc, year, month, day) in test_cases {
+        let date = UTCDate::from_utc_days(day_utc);
+        let expected = UTCDate {
+            year,
+            month,
+            day
+        };
+        assert_eq!(date, expected);
+    }
+
+    Ok(())
 }
