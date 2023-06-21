686e773a2b836ea0197b12e87d1452bbc2ed9833
Reece Kibble <reecek@uniciant.com>
2023-06-21 04:56:36 +0000

    Doc update for new conversions and method names

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //! [dependencies]
 //! utc-dt = "0.1"
 //! ```
-//! For extended/niche features and local timezone support see [`chrono`](https://github.com/chronotope/chrono) or [`time`](https://github.com/time-rs/time).
+//! For extended/niche features and local time-zone support see [`chrono`](https://github.com/chronotope/chrono) or [`time`](https://github.com/time-rs/time).
 //!
 //! ### NOTE
 //! Only capable of expressing times and dates SINCE the Unix Epoch `(1970-01-01T00:00:00Z)`. This library takes advantage of this assumption to simplify the API and internal logic.
@@ -48,16 +48,16 @@
 //!     // Not available for #![no_std]
 //!     let utc_timestamp = UTCTimestamp::try_from_system_time().unwrap();
 //!     // UTC Timestamp from a u64 measurement directly.
-//!     let utc_timestamp = UTCTimestamp::from_millis(1686824288903);
+//!     let utc_timestamp: UTCTimestamp = Duration::from_millis(1686824288903).into();
 //!     // Use UTC Timestamp to get time-of-day
-//!     let time_of_day_ns: u64 = utc_timestamp.to_time_of_day_ns();
+//!     let time_of_day_ns: u64 = utc_timestamp.as_time_of_day_ns();
 //!     // Use UTC Timestamp to get days since epoch (ie. UTC Day)
-//!     let utc_day: UTCDay = utc_timestamp.to_utc_day();
+//!     let utc_day: UTCDay = utc_timestamp.as_utc_day();
 //!
 //!     // UTC Day from an integer
 //!     let utc_day = UTCDay::from(19523);
 //!     // Use UTC Day to get the weekday
-//!     let weekday = utc_day.to_utc_weekday();
+//!     let weekday = utc_day.as_utc_weekday();
 //!
 //!     // UTC Date directly from components
 //!     let utc_date = UTCDate::try_from_components(2023, 6, 15).unwrap();
@@ -68,13 +68,13 @@
 //!     // Get number of days within date's month
 //!     let days_in_month: u8 = utc_date.days_in_month();
 //!     // Get the date in integer forms
-//!     let (year, month, day) = (utc_date.year(), utc_date.month(), utc_date.day()); // OR
-//!     let (year, month, day) = utc_date.to_components();
+//!     let (year, month, day) = (utc_date.as_year(), utc_date.as_month(), utc_date.as_day()); // OR
+//!     let (year, month, day) = utc_date.as_components();
 //!     // UTC Day from UTC Date
-//!     let utc_day = utc_date.to_utc_day();
+//!     let utc_day = utc_date.as_utc_day();
 //!     // Get date string formatted according to ISO 8601 `(YYYY-MM-DD)`
 //!     // Not available with `no_std`
-//!     let iso_date = utc_date.to_iso_date();
+//!     let iso_date = utc_date.as_iso_date();
 //!     assert_eq!(iso_date, "2023-06-15");
 //!
 //!     // UTC Datetime directly from raw components
@@ -87,15 +87,15 @@
 //!     // UTC Datetime from date and time-of-day components
 //!     let utc_datetime = UTCDatetime::try_from_components(utc_date, time_of_day_ns).unwrap();
 //!     // Get date and time-of-day components
-//!     let (utc_date, time_of_day_ns) = (utc_datetime.to_date(), utc_datetime.to_time_of_day_ns());
-//!     let (utc_date, time_of_day_ns) = utc_datetime.to_components();
+//!     let (utc_date, time_of_day_ns) = (utc_datetime.as_date(), utc_datetime.as_time_of_day_ns());
+//!     let (utc_date, time_of_day_ns) = utc_datetime.as_components();
 //!     // Get the time in hours, minutes and seconds
-//!     let (hours, minutes, seconds) = utc_datetime.to_hours_minutes_seconds();
+//!     let (hours, minutes, seconds) = utc_datetime.as_hours_minutes_seconds();
 //!     // Get the sub-second component of the time of day, in nanoseconds
-//!     let subsec_ns = utc_datetime.to_subsec_ns();
+//!     let subsec_ns = utc_datetime.as_subsec_ns();
 //!     // Get UTC datetime string formatted according to ISO 8601 `(YYYY-MM-DDThh:mm:ssZ)`
 //!     // Not available with `no_std`
-//!     let iso_datetime = utc_datetime.to_iso_datetime();
+//!     let iso_datetime = utc_datetime.as_iso_datetime();
 //!     assert_eq!(iso_datetime, "2023-06-15T10:18:08Z");
 //!
 //!     {
@@ -129,6 +129,14 @@
 //!         let utc_day = UTCDay::from_utc_secs(1686824288);
 //!         let utc_date = UTCDate::from_utc_millis(1686824288_000);
 //!         let utc_datetime = UTCDate::from_utc_micros(1686824288_000_000);
+//!
+//!         // Convert from UTC Day / UTC Date / UTC Datetime back to various types
+//!         let utc_duration: Duration = utc_day.as_utc_duration();
+//!         let utc_timestamp: UTCTimestamp = utc_date.as_utc_timestamp();
+//!         let utc_secs: u64 = utc_date.as_utc_secs();
+//!         let utc_millis: u64 = utc_datetime.as_utc_millis();
+//!         let utc_micros: u64 = utc_day.as_utc_micros();
+//!         let utc_nanos: u64 = utc_date.as_utc_nanos();
 //!     }
 //! ```
 //!
