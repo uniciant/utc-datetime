b9756d8ab3a540a04bb7a2b9d2804bd1a0329579
reece <reecek@uniciant.com>
2024-08-26 19:41:24 +0800

    add docs badge

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 //!
 //! [![crates.io](https://img.shields.io/crates/v/utc-dt?style=flat-square&logo=rust)](https://crates.io/crates/utc-dt)
 //! [![license](https://img.shields.io/badge/license-Apache--2.0_OR_MIT-blue?style=flat-square)](#license)
+//! [![docs](https://img.shields.io/docsrs/utc-dt/latest)](https://docs.rs/utc-dt)
 //! [![build status](https://img.shields.io/github/actions/workflow/status/uniciant/utc-datetime/rust.yml?branch=main&style=flat-square&logo=github)](https://github.com/uniciant/utc-datetime/actions)
 //! [![codecov](https://codecov.io/gh/uniciant/utc-datetime/branch/main/graph/badge.svg?token=XTOHZ187TY)](https://codecov.io/gh/uniciant/utc-datetime)
 //!
